46cc012252a071f6cbcf5108e7042a5eb8e37001
MatteoFilippini <matteofilippini8@gmail.com>
2022-04-27 20:57:10 +0200

    upload flat image

--- resources/views/admin/flats/show.blade.php
+++ resources/views/admin/flats/show.blade.php
@@ -5,8 +5,11 @@
     <h1>
         {{$flat->title}}
     </h1>
+    <p>
+        {{$flat->square_meters}} mq
+    </p>
+    <div>
+    <img src="{{asset('storage/'.$flat->default_image)}}" alt="cover_image" style="width: 400px;height:400px">
+    </div>
 </div>
-<p>
-    {{$flat->square_meters}} mq
-</p>
 @endsection
