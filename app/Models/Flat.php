1b362a05b3c3d9fe2d807b73463de23c968dc563
AdrianaSantorio <adriana.santorio@gmail.com>
2022-04-27 09:50:02 +0200

    tried to add package for views

--- app/Models/Flat.php
+++ app/Models/Flat.php
@@ -3,30 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use CyrildeWit\EloquentViewable\InteractsWithViews;
+use CyrildeWit\EloquentViewable\Contracts\Viewable;
 
-class Flat extends Model
+class Flat extends Model implements Viewable
 {
-    public function user(){
+    use InteractsWithViews;
+
+    public function user()
+    {
         return $this->belongsTo('App\User');
     }
 
-    public function address(){
+    public function address()
+    {
         return $this->hasOne('App\Models\Address');
     }
 
-    public function messages(){
+    public function messages()
+    {
         return $this->hasMany('App\Models\Message');
     }
 
-    public function images(){
+    public function images()
+    {
         return $this->hasMany('App\Models\Image');
     }
 
-    public function services(){
+    public function services()
+    {
         return $this->belongsToMany('App\Models\Service');
     }
 
-    public function sponsors(){
+    public function sponsors()
+    {
         return $this->belongsToMany('App\Models\Sponsor');
     }
 }
