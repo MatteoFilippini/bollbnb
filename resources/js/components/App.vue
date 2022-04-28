564477d515efe4ea870e7719cf963493b8bc985c
maino1997 <sashamainardi4@gmail.com>
2022-04-28 18:04:17 +0200

    links from and to login and validation in jquery on register form

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
-      
+      <Header />
       <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from './Header.vue'
 export default {
+  components: { Header },
 name:'App'
 }
 </script>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <nav>
+        <a href="/admin">Accedi</a>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+name:'Header'
+}
+</script>
+
+<style scoped lang='scss'>
+nav{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
