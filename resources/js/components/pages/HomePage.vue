004e8e46c90d837134e2b53cfa3d11dc2a508604
MatteoFilippini <matteofilippini8@gmail.com>
2022-04-30 22:15:56 +0200

    add loader

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -2,39 +2,36 @@
   <div class="container">
     <h1>Home</h1>
     <h2 class="text-center">Appartamenti Sponsor</h2>
-    <!-- <h4 v-for="flat_sponsor in flats_sponsor" :key="flat_sponsor.id">
-      {{ flat_sponsor.title }}
-    </h4>  -->
     <FlatCard
       v-for="flat_sponsor in flats_sponsor"
       :key="flat_sponsor.id"
       :flat="flat_sponsor"
     />
-
+    <Loader v-if="isLoading" />
     <h2 class="text-center">Tutti gli Appartamenti</h2>
-    <!-- <h4 v-for="flat in flats" :key="flat.id">
-      {{ flat.title }}
-    </h4> -->
-
     <FlatCard v-for="flat in flats" :key="flat.id" :flat="flat" />
   </div>
 </template>
 
 <script>
 import FlatCard from "../flats/FlatCard.vue";
+import Loader from "../Loader.vue";
 export default {
   name: "HomePage",
   components: {
     FlatCard,
+    Loader,
   },
   data() {
     return {
       flats_sponsor: [],
       flats: [],
+      isLoading: false,
     };
   },
   methods: {
     IndexFlats() {
+      this.isLoading = true;
       axios
         .get("http://localhost:8000/api/flats")
         .then((res) => {
@@ -46,7 +43,7 @@ export default {
         })
         .then(() => {
           this.isLoading = false;
-          console.log("Api terminata");
+          console.log("index terminata");
         });
     },
   },
