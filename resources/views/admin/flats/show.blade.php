bf5ffdbe1aead4f4623f4c80dcfff0ce3e778e12
MatteoFilippini <matteofilippini8@gmail.com>
2022-05-02 14:04:34 +0200

    add messages in show flat

--- resources/views/admin/flats/show.blade.php
+++ resources/views/admin/flats/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 <div class="container">
+    <h1> {{$flat->id}}</h1>
     <h1>
         <div class="d-flex justify-content-between">
             {{$flat->title}}
@@ -12,10 +13,25 @@
         {{$flat->square_meters}} mq
     </p>
     <div>
-    {{$flat->description}} 
+        {{$flat->description}}
     </div>
     <div>
-    <img src="{{asset('storage/'.$flat->default_image)}}" alt="cover_image" style="width: 400px;height:400px">
+        <img src="{{asset('storage/'.$flat->default_image)}}" alt="cover_image" style="width: 400px;height:400px">
     </div>
+
+    <h2>Messaggi ricevuti:</h2>
+
+    @forelse($message as $m)
+    <div>
+        da: {{$m->sender_mail}}
+        contenuto: {{$m->content}}
+    </div>
+    @empty
+    <div>
+        Non hai ricevuto ancora messaggi
+    </div>
+    @endforelse
+
+
 </div>
 @endsection
