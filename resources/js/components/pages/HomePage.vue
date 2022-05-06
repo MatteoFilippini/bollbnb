c9a669ef1b0d72c4bad9bfaf5da1085a5af0590e
niculas1989 <nicolas.maranzano@libero.it>
2022-05-06 16:12:58 +0200

    ulteriore stile - front

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="container">
-    <h1>Home</h1>
-    <h2 class="text-center">Appartamenti Sponsor</h2>
-    <FlatCard
-      v-for="flat_sponsor in flats_sponsor"
-      :key="flat_sponsor.id"
-      :flat="flat_sponsor"
-    />
-    <Loader v-if="isLoading" />
-    <h2 class="text-center">Tutti gli Appartamenti</h2>
-    <FlatCard v-for="flat in flats" :key="flat.id" :flat="flat" />
+  <div class="container-fluid">
+    <div class="container">
+      <!-- sezione di benvenuto -->
+      <section id="info" class="text-center my-5 rounded text-dark">
+        <div class="h-100 d-flex justify-content-center align-items-center">
+          <h1>BENVENUTO SU BOOLBNB</h1>
+        </div>
+      </section>
+
+      <!-- sezione con immagine esempio più link ad eventuale registrazione -->
+      <section id="example">
+        <div class="mb-4">
+          <div id="bottom-items" class="text-center">
+            <p>Vivi la tua esperienza</p>
+            <a href="#" class="btn">PROVA</a>
+          </div>
+        </div>
+      </section>
+      <h2 class="text-center">Appartamenti Sponsor</h2>
+      <FlatCard
+        v-for="flat_sponsor in flats_sponsor"
+        :key="flat_sponsor.id"
+        :flat="flat_sponsor"
+      />
+      <Loader v-if="isLoading" />
+      <h2 class="text-center">Tutti gli Appartamenti</h2>
+      <FlatCard v-for="flat in flats" :key="flat.id" :flat="flat" />
+    </div>
   </div>
 </template>
 
