6c9eb6f563cb1b39ba06f9d39510b52851c4e1d2
MatteoFilippini <matteofilippini8@gmail.com>
2022-05-08 21:47:22 +0200

    change services navbar

--- resources/js/components/Loader.vue
+++ resources/js/components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="d-flex justify-content-center">
+  <div class="d-flex justify-content-center align-items-center load">
     <div class="spinner-border" style="width: 5rem; height: 5rem" role="status">
       <span class="sr-only">Loading...</span>
     </div>
@@ -12,5 +12,8 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.load{
+  height:100vh
+}
 </style>
