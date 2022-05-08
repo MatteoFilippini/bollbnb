4daa508ad3b596e8d49fa9d34e8872720a042cd1
niculas1989 <nicolas.maranzano@libero.it>
2022-05-08 21:08:45 +0200

    modifiche

--- resources/js/components/flats/FlatCard.vue
+++ resources/js/components/flats/FlatCard.vue
@@ -1,10 +1,18 @@
 <template>
-  <div v-if="flat">   
-    <div class="border border-light">
-     <h1>{{flat.id}}</h1> 
+  <div v-if="flat">
+    <div id="image-wrapper">
+      <img
+        :src="flat.default_image"
+        alt="immagine appartamento"
+        class="img-fluid rounded"
+      />
+    </div>
+
+    <div class="">
+      <h1>{{ flat.id }}</h1>
       <h1>{{ flat.title }}</h1>
       <!-- <img :src="flat.default_image" alt="immagine"> -->
-      <p>{{flat.default_image}}</p>
+      <p>{{ flat.default_image }}</p>
       <p>{{ flat.description }}</p>
       <!-- <h3>{{ flat.user.name }}</h3> -->
       <router-link
@@ -12,28 +20,30 @@
         class="btn btn-secondary btn-sm"
         v-if="!isShow"
         >Dettaglio
-        </router-link>
+      </router-link>
 
-        <!-- SE SIAMO NELLA RICERCA VEDIAMO ANCHE I SERVIZI COSI -->
-        <div v-if="isSearch">
-           <h3>Servizi:</h3>
-          <ul>
-            <li v-for="service in flat.services" :key="service.id">{{service.type}}</li>
-          </ul>          
-        </div>
+      <!-- SE SIAMO NELLA RICERCA VEDIAMO ANCHE I SERVIZI COSI -->
+      <div v-if="isSearch">
+        <h3>Servizi:</h3>
+        <ul>
+          <li v-for="service in flat.services" :key="service.id">
+            {{ service.type }}
+          </li>
+        </ul>
+      </div>
     </div>
 
     <!-- SE SIAMO INVECE NEL DETTAGLIO VEDIAMO QUESTI BOTTONI -->
-  <div  v-if="isShow">
-    <button class="btn btn-danger btn-sm" onClick="window.history.back()">Indietro</button>
-    <router-link
-      :to="{ name: 'messageForm', params: { id: flat.id } }"
-      class="btn btn-primary btn-sm"
-     
-      >messaggio</router-link
-    >
-
-  </div>
+    <div v-if="isShow">
+      <button class="btn btn-danger btn-sm" onClick="window.history.back()">
+        Indietro
+      </button>
+      <router-link
+        :to="{ name: 'messageForm', params: { id: flat.id } }"
+        class="btn btn-primary btn-sm"
+        >messaggio</router-link
+      >
+    </div>
     <!-- <router-link
       :to="{ name: 'home' }"
       class="btn btn-danger btn-sm"
@@ -41,16 +51,19 @@
       >Indietro
       </router-link
     > -->
-
   </div>
 </template>
 
 <script>
 export default {
   name: "FlatCard",
-  props: ["flat", "is-show","is-search"],
+  props: ["flat", "is-show", "is-search"],
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+#image-wrapper {
+  height: 400px;
+  width: 400px;
+}
 </style>
