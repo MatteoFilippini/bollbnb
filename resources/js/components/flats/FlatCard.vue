b73d722403cd2223352a40c90e9316d01e301220
niculas1989 <nicolas.maranzano@libero.it>
2022-05-10 12:23:55 +0200

    me la fa mandare su ma non ho toccato nulla

--- resources/js/components/flats/FlatCard.vue
+++ resources/js/components/flats/FlatCard.vue
@@ -4,24 +4,26 @@
     <div v-if="isSearch" class="flat-search">
       <div class="row">
         <div class="col-5 image-flat">
-          <img :src="flat.default_image" alt="">
+          <img :src="flat.default_image" alt="" />
         </div>
         <div class="col-7 desc-flat">
           <div class="flat-search-title">
-            <h3>{{flat.title}}</h3>
+            <h3>{{ flat.title }}</h3>
           </div>
           <div class="flat-search-details mb-5">
-            <p class="text-muted">Ospiti: {{flat.beds}} - Camere: {{flat.rooms}} - Bagni: {{flat.bathrooms}}
-              <br> Metri quadrati: {{flat.square_meters}}
+            <p class="text-muted">
+              Ospiti: {{ flat.beds }} - Camere: {{ flat.rooms }} - Bagni:
+              {{ flat.bathrooms }} <br />
+              Metri quadrati: {{ flat.square_meters }}
             </p>
           </div>
           <div v-if="flat.services.length">
             <h5>Servizi:</h5>
-              <ul>
-                <li v-for="service in flat.services" :key="service.id">
-                  {{ service.type }}
-                </li>
-              </ul>
+            <ul>
+              <li v-for="service in flat.services" :key="service.id">
+                {{ service.type }}
+              </li>
+            </ul>
           </div>
           <div v-else>
             <h5>Non ci sono servizi</h5>
@@ -38,38 +40,45 @@
     <!-- FLAT PAGINA SHOW -->
     <div v-if="isShow">
       <div class="flat-show">
-      <h1>{{flat.title}}</h1>
-      <h3>INDIRIZZO</h3> 
-      <div class="row border border-light mb-5">
-        <div class="col-sm-12 col-lg-6 main border border-primary">DEAFULT IMAGE</div>
-        <div class="col-xs-6 col-sm-6 col-lg-3 images border border-success">SE CE ALTRA IMMAGINE</div>
-        <div class="col-xs-6 col-sm-6 col-lg-3 images border border-success">SE CE ALTRA IMMAGINE</div>
-      </div>
-      <h3>Host: {{flat.user.name}}</h3>
-      <h5>{{flat.description}}</h5>
-      <p class="text-muted">{{flat.beds}} ospiti-{{flat.rooms}} stanze-{{flat.bathrooms}} bagni-{{flat.sqare_meters}} metri</p>
-      <hr class="border border-light w-20"/>
-      <h3>Cosa troverai:</h3>
-      QUI METTEREMO LA LISTA DEI SERVIZI 
-      <div class="buttons">
-      <router-link
-          :to="{ name: 'home' }"
-          class="button-b"
-        >
-          Indietro
-        </router-link>
-      <router-link
-          :to="{ name: 'messageForm', params: { id: flat.id } }"
-          class="message"
-        >
-          Contatta l'host
-        </router-link>
+        <h1>{{ flat.title }}</h1>
+        <h3>INDIRIZZO</h3>
+        <div class="row border border-light mb-5">
+          <div class="col-sm-12 col-lg-6 main border border-primary">
+            DEAFULT IMAGE
+          </div>
+          <div class="col-xs-6 col-sm-6 col-lg-3 images border border-success">
+            SE CE ALTRA IMMAGINE
+          </div>
+          <div class="col-xs-6 col-sm-6 col-lg-3 images border border-success">
+            SE CE ALTRA IMMAGINE
+          </div>
+        </div>
+        <h3>Host: {{ flat.user.name }}</h3>
+        <h5>{{ flat.description }}</h5>
+        <p class="text-muted">
+          {{ flat.beds }} ospiti-{{ flat.rooms }} stanze-{{
+            flat.bathrooms
+          }}
+          bagni-{{ flat.sqare_meters }} metri
+        </p>
+        <hr class="border border-light w-20" />
+        <h3>Cosa troverai:</h3>
+        QUI METTEREMO LA LISTA DEI SERVIZI
+        <div class="buttons">
+          <router-link :to="{ name: 'home' }" class="button-b">
+            Indietro
+          </router-link>
+          <router-link
+            :to="{ name: 'messageForm', params: { id: flat.id } }"
+            class="message"
+          >
+            Contatta l'host
+          </router-link>
+        </div>
       </div>
     </div>
-
   </div>
-</div>
-    <!-- <div v-if="isSearch">
+  <!-- <div v-if="isSearch">
         SEARCH
     </div>
     <div class="col-sm-12 col-md-12 col-lg-3 py-4">
@@ -141,18 +150,18 @@ export default {
 }
 
 // search
-.flat-search{
-  padding:30px;
+.flat-search {
+  padding: 30px;
   border-bottom: 1px solid white;
   position: relative;
-  .image-flat{
-    background-color:green;
-    border-radius:20px;
+  .image-flat {
+    background-color: green;
+    border-radius: 20px;
     height: 250px;
     width: 100%;
     min-width: 200px;
   }
-  .detail-bottom{
+  .detail-bottom {
     position: absolute;
     right: 0;
     bottom: 0;
@@ -160,47 +169,47 @@ export default {
 }
 
 // show
-.flat-show{
+.flat-show {
   position: relative;
-  .main{
+  .main {
     height: 400px;
   }
-  .images{
-    height:200px;
+  .images {
+    height: 200px;
   }
-  p{
-    font-size:20px;
+  p {
+    font-size: 20px;
   }
-  h3{
+  h3 {
     font-weight: 600;
-    margin-bottom:20px;
+    margin-bottom: 20px;
   }
-  .w-20{
-    width:20%;
+  .w-20 {
+    width: 20%;
   }
-  .buttons{
+  .buttons {
     position: absolute;
     bottom: 0;
     right: 0;
-    display:flex;
+    display: flex;
     justify-content: center;
     align-items: center;
-    .button-b{
-      font-size:1rem;
+    .button-b {
+      font-size: 1rem;
       margin-right: 17px;
     }
-    .message{
-        border: 1px solid #fff;
-        background-color: black;
-        color: white;
-        padding:7px 15px;
-        border-radius:10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        text-decoration: none;
-        font-size: 1.1rem;
-        max-width: 200px;
+    .message {
+      border: 1px solid #fff;
+      background-color: black;
+      color: white;
+      padding: 7px 15px;
+      border-radius: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      font-size: 1.1rem;
+      max-width: 200px;
     }
   }
 }
